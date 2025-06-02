a2289fd09a200ee5c2466b446f7af0bfec6e946c
KreshMetal <70015861+KreshMetal@users.noreply.github.com>
2025-06-02 12:35:05 +0300

    Update log.h

--- BaseBin/libjailbreak/src/roothider/log.h
+++ BaseBin/libjailbreak/src/roothider/log.h
@@ -1,6 +1,5 @@
 #define ENABLE_LOGS
 
-#ifdef ENABLE_LOGS
 #include <unistd.h>
 #include <stdbool.h>
 
@@ -16,8 +15,3 @@ void JBLogFunction(const char* path, pid_t pid, uint64_t tid, const char* prefix
 
 #define JBLogDebug(...) do { if(true) JBLogDebugFunction(__VA_ARGS__); } while(0)
 #define JBLogError(...) do { if(true) JBLogErrorFunction(__VA_ARGS__); } while(0)
-
-#else
-#define JBLogDebug(...)
-#define JBLogError(...)
-#endif
